bebaa44e37dce515e46c2e877cc371d8b2a91075
Yosef da Iry <yosefda@gmail.com>
2015-12-29 01:34:03 +1100

    cc validation done

--- hw1-cc-validation.hsproj/HShw1_cc_validation.hs
+++ hw1-cc-validation.hsproj/HShw1_cc_validation.hs
@@ -11,3 +11,25 @@ toDigits i
 -- convert positive Integers to a reversed list of digits
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev i = reverse (toDigits i)
+
+-- double every other number beginning from the right
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther xs = reverse (doDoubleEveryOther (reverse xs))
+
+-- helper function that actually performs double every other element
+doDoubleEveryOther :: [Integer] -> [Integer]
+doDoubleEveryOther [] = []
+doDoubleEveryOther (x1:[]) = x1:[]
+doDoubleEveryOther (x1:x2:[]) = x1:(x2*2):[]
+doDoubleEveryOther (x1:x2:xs) = x1:(x2*2):doDoubleEveryOther(xs)
+
+-- sum of the original numbers of the digits
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits xss = sum (concat [toDigits xs | xs <- xss])
+
+-- validate cc numbers
+validate :: Integer -> Bool
+validate n = if sumDigits (doubleEveryOther (toDigits n)) `mod` 10 == 0 then True else False
+ 
